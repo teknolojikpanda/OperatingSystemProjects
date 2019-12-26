7e8a7f2a9e8ba69579925f13cb54d13754bc53a0
Can Doğa Uçak <dogaucak@gmail.com>
2019-12-26 02:27:33 +0200

    2. Update
    program can take external input from storage

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define SIZE 5
+#define LINE_MAX 1024
 
 typedef struct linkedList {
 
@@ -12,20 +16,20 @@ typedef struct linkedList {
     int waitingTime;
 }linkedList;
 
-void FCFS(linkedList process[],int size){
+void FCFS(linkedList process[]){
 
-    linkedList temp[10];
+    linkedList temp[SIZE];
     int i;
-    int totlWaiting=0;
+    int totalWaiting=0;
     double averageWaiting;
 
-    for(i=0;i<size;i++)
+    for(i=0;i<SIZE;i++)
         temp[i]=process[i];
 
     linkedList list;
     int a,b;
-    for(a=1;a<size;a++) {
-        for (b = 0; b < size - a; b++) {
+    for(a=1;a<SIZE;a++) {
+        for (b = 0; b < SIZE - a; b++) {
             if (temp[b].arrivalTime > temp[b + 1].arrivalTime) {
                 list = temp[b];
                 temp[b] = temp[b + 1];
@@ -34,40 +38,80 @@ void FCFS(linkedList process[],int size){
         }
     }
 
-    totlWaiting = temp[0].waitingTime = 0;
+    totalWaiting = temp[0].waitingTime = 0;
 
-    for(i=1;i<size;i++){
+    for(i = 1; i < SIZE; i++){
         temp[i].waitingTime = (temp[i-1].burstTime + temp[i-1].arrivalTime + temp[i-1].waitingTime) - temp[i].arrivalTime;
-        totlWaiting+=temp[i].waitingTime;
+        totalWaiting += temp[i].waitingTime;
     }
-    averageWaiting = (double)totlWaiting/size;
+    averageWaiting = (double)totalWaiting/SIZE;
 
     printf("Scheduling Method : First Come First Served\nProcess Waiting Times:");
-    for(i=0;i<size;i++) {
+    for(i = 0; i < SIZE; i++) {
         printf("\nP%d: %d ms", i+1, temp[i].waitingTime);
     }
     printf("\nAverage waiting time: %f ms\n",averageWaiting);
 }
 
-int main() {
-    struct linkedList process[5];
-    process[0].burstTime = 5;
-    process[1].burstTime = 4;
-    process[2].burstTime = 3;
-    process[3].burstTime = 4;
-    process[4].burstTime = 3;
-    process[0].arrivalTime = 0;
-    process[1].arrivalTime = 1;
-    process[2].arrivalTime = 1;
-    process[3].arrivalTime = 2;
-    process[4].arrivalTime = 3;
-    process[0].priority = 3;
-    process[1].priority = 2;
-    process[2].priority = 1;
-    process[3].priority = 2;
-    process[4].priority = 1;
+void SJFS_nonpreemptive(linkedList process[]){
 
-    FCFS(process,5);
+}
+
+void SJFS_preemptive(linkedList process[]){
+
+}
+
+void PS_preemptive(linkedList process[]){
+
+}
+
+void PS_nonpreemptive(linkedList process[]){
+
+}
+
+void RR(linkedList process[]){
+
+}
+
+int main(int argc, char **argv) {
+    struct linkedList process[SIZE];
+    int i = 0;
+    int choice;
+    FILE *fp;
+    char line[LINE_MAX];
+    unsigned int num[3];
+
+    opterr = 0;
+
+    while ((choice = getopt (argc, argv, "f:o:")) != -1)
+        switch (choice)
+        {
+            case 'f':
+                if ((fp = fopen(optarg, "r")) == NULL)
+                    return 0;
+                while (fgets(line, LINE_MAX, fp) != NULL) {
+                    sscanf(line,"%d:%d:%d\n",&num[0],&num[1],&num[2]);
+                    process[i].burstTime = num[0];
+                    process[i].arrivalTime = num[1];
+                    process[i].priority = num[2];
+                    i++;
+                }
+                fclose(fp);
+                break;
+            case 'o':
+
+                break;
+            default:
+                abort ();
+        }
+
+    FCFS(process);
+    //SJFS_nonpreemptive(process);
+    //SJFS_preemptive(process);
+    //PS_nonpreemptive(process);
+    //PS_preemptive(process);
+    //RR(process);
 
     return 0;
 }
+
